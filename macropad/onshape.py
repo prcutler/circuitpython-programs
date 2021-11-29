e0d8d68ecf213ef162cc367e3ebc4fec13da8712
Paul Cutler <paul@paulcutler.org>
2021-11-28 22:15:00 -0600

    Fix undo again

--- macropad/onshape.py
+++ macropad/onshape.py
@@ -11,7 +11,7 @@ app = {                    # REQUIRED dict, must be named 'app'
         (0x004000, 'Front', [Keycode.SHIFT, '1']), # Front View
         (0x400000, 'Back', [Keycode.SHIFT, '2 ']),      # Back View
         # 2nd row ----------
-        (0x202000, 'Undo', [Keycode.SHIFT, 'Z'), # Undo
+        (0x202000, 'Undo', [Keycode.SHIFT, 'Z']), # Undo
         (0x202000, 'Up', [UP_ARROW]), # Up
         (0x400000, 'Planes', 'P'),   #  Hide / Show Planes
         # 3rd row ----------
@@ -24,3 +24,5 @@ app = {                    # REQUIRED dict, must be named 'app'
         (0x004000, 'Search', [Keycode.OPTION, 'C']), # Search Tools
         # Encoder button ---
         (0x000000, '', [Keycode.COMMAND, 'w']) # Close window/tab
+]
+}
