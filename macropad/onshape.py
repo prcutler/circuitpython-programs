15eb0ca458ab5f0a1262db1bf0abd607a5fd32ba
Paul Cutler <paul@paulcutler.org>
2021-11-29 15:15:20 -0600

    Update colors - macro complete

--- macropad/onshape.py
+++ macropad/onshape.py
@@ -7,21 +7,21 @@ app = {                    # REQUIRED dict, must be named 'app'
     'macros'   : [           # List of button macros...
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x004000, 'Iso', [Keycode.SHIFT, '7']), # Isometric (Home) View
-        (0x004000, 'Front', [Keycode.SHIFT, '1']), # Front View
-        (0x3a5fcd, 'Undo', [Keycode.SHIFT, 'Z']),  # Undo
+        (0x228b22, 'Iso', [Keycode.SHIFT, '7']), # Isometric (Home) View - Forest Green
+        (0x191970, 'Front', [Keycode.SHIFT, '1']), # Front View
+        (0xff0800, 'Undo', [Keycode.SHIFT, 'Z']),  # Undo - Candy Apple Red
         # 2nd row ----------
-        (0x004000, 'Left', [Keycode.SHIFT, '3']),  # Left View
-        (0x3a5fcd, 'Back', [Keycode.SHIFT, '2']),      # Back View
-        (0x202000, 'Right', [Keycode.SHIFT, '4']),  # Right View
+        (0x191970, 'Left', [Keycode.SHIFT, '3']),  # Left View
+        (0x191970, 'Back', [Keycode.SHIFT, '2']),      # Back View
+        (0x191970, 'Right', [Keycode.SHIFT, '4']),  # Right View
         # 3rd row ----------
-        (0x000040, 'Zoom', 'F'),  # Zoom to fit
-        (0x000040, 'Up', [Keycode.UP_ARROW]), # Up arrow
-        (0xffd800, 'Planes', 'P'),  # Hide / Show Planes
+        (0x228b22, 'Zoom', 'F'),  # Zoom to fit
+        (0x000080, 'Up', [Keycode.UP_ARROW]), # Up arrow
+        (0xffef00, 'Planes', 'P'),  # Hide / Show Planes - Canary Yellow
         # 4th row ----------
-        (0x000040, 'Left', [Keycode.LEFT_ARROW]),  # Left
-        (0x000040, 'Down', [Keycode.DOWN_ARROW]),  # Down
-        (0x000040, 'Right', [Keycode.RIGHT_ARROW]),  # Right
+        (0x000080, 'Left', [Keycode.LEFT_ARROW]),  # Left
+        (0x000080, 'Down', [Keycode.DOWN_ARROW]),  # Down
+        (0x000080, 'Right', [Keycode.RIGHT_ARROW]),  # Right
         # Encoder button ---
         # (0x000000, '', [Keycode.COMMAND, 'w']) # Close window/tab
 ]
