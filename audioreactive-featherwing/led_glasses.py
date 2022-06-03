7196bc368a5b7f4077461e8807ba263e78631706
Paul Cutler <paul@paulcutler.org>
2022-06-02 20:34:49 -0500

    Start to port audio reactive project from NeoPixel FeatherWing to MatrixPortal

--- audioreactive-featherwing/led_glasses.py
+++ audioreactive-featherwing/led_glasses.py
@@ -1,21 +1,25 @@
-import time
-from math import sin
-import board
-import displayio
-import rgbmatrix
-import framebufferio
+# SPDX-FileCopyrightText: 2021 Phil Burgess for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
 
-from audiobusio import PDMIn
-from ulab import numpy as np
-from ulab.scipy.signal import spectrogram
-from rainbowio import colorwheel
+"""
+AUDIO SPECTRUM LIGHT SHOW for Adafruit EyeLights (LED Glasses + Driver).
+Uses onboard microphone and a lot of math to react to music.
+"""
 
 from array import array
 from math import log
-import audiobusio
-import time
+from time import monotonic
+from supervisor import reload
+import board
+from audiobusio import PDMIn
+from busio import I2C
+import adafruit_is31fl3741
+from adafruit_is31fl3741.adafruit_rgbmatrixqt import Adafruit_RGBMatrixQT
+from rainbowio import colorwheel
+from ulab import numpy as np
+from ulab.scipy.signal import spectrogram
 
-import adafruit_framebuf as pixel_framebuf
 
 # FFT/SPECTRUM CONFIG ----
 
@@ -23,37 +27,37 @@ fft_size = 256  # Sample size for Fourier transform, MUST be power of two
 spectrum_size = fft_size // 2  # Output spectrum is 1/2 of FFT result
 # Bottom of spectrum tends to be noisy, while top often exceeds musical
 # range and is just harmonics, so clip both ends off:
-# Original low = 10 and high = 75
-low_bin = 20
-high_bin = 40  # Highest bin "
-
-pixel_width = 64
-pixel_height = 32
-
-# Initialize hardware
-displayio.release_displays()
-
-matrix = rgbmatrix.RGBMatrix(
-    width=64, height=32, bit_depth=4,
-    rgb_pins=[board.MTX_R1,
-              board.MTX_G1,
-              board.MTX_B1,
-              board.MTX_R2,
-              board.MTX_G2,
-              board.MTX_B2],
-    addr_pins=[board.MTX_ADDRA,
-               board.MTX_ADDRB,
-               board.MTX_ADDRC,
-               board.MTX_ADDRD],
-    clock_pin=board.MTX_CLK, latch_pin=board.MTX_LAT,
-    output_enable_pin=board.MTX_OE)
+low_bin = 10  # Lowest bin of spectrum that contributes to graph
+high_bin = 75  # Highest bin "
 
 
-mic = audiobusio.PDMIn(board.D10, board.D9,
-                       sample_rate=16000, bit_depth=16)
+# HARDWARE SETUP ---------
+
+# Manually declare I2C (not board.I2C() directly) to access 1 MHz speed...
+i2c = I2C(board.SCL, board.SDA, frequency=1000000)
+
+# Initialize the IS31 LED driver, buffered for smoother animation
+#glasses = LED_Glasses(i2c, allocate=adafruit_is31fl3741.MUST_BUFFER)
+glasses = Adafruit_RGBMatrixQT(i2c, allocate=adafruit_is31fl3741.MUST_BUFFER)
+
+glasses.show()  # Clear any residue on startup
+glasses.set_led_scaling(0xFF)
+glasses.global_current = 5  # Not too bright please
+glasses.enable = True
+
+# Initialize mic and allocate recording buffer (default rate is 16 MHz)
+mic = PDMIn(board.MICROPHONE_CLOCK, board.MICROPHONE_DATA, bit_depth=16)
 rec_buf = array("H", [0] * fft_size)  # 16-bit audio samples
 
 
+# FFT/SPECTRUM SETUP -----
+
+# To keep the display lively, tables are precomputed where each column of
+# the matrix (of which there are few) is the sum value and weighting of
+# several bins from the FFT spectrum output (of which there are many).
+# The tables also help visually linearize the output so octaves are evenly
+# spaced, as on a piano keyboard, whereas the source spectrum data is
+# spaced by frequency in Hz.
 column_table = []
 
 spectrum_bits = log(spectrum_size, 2)  # e.g. 7 for 128-bin spectrum
@@ -61,12 +65,12 @@ spectrum_bits = log(spectrum_size, 2)  # e.g. 7 for 128-bin spectrum
 low_frac = log(low_bin, 2) / spectrum_bits
 frac_range = log(high_bin, 2) / spectrum_bits - low_frac
 
-for column in range(pixel_width):
+for column in range(glasses.width):
     # Determine the lower and upper frequency range for this column, as
     # fractions within the scaled 0.0 to 1.0 spectrum range. 0.95 below
     # creates slight frequency overlap between columns, looks nicer.
-    lower = low_frac + frac_range * (column / pixel_width * 0.95)
-    upper = low_frac + frac_range * ((column + 1) / pixel_width)
+    lower = low_frac + frac_range * (column / glasses.width * 0.95)
+    upper = low_frac + frac_range * ((column + 1) / glasses.width)
     mid = (lower + upper) * 0.5  # Center of lower-to-upper range
     half_width = (upper - lower) * 0.5  # 1/2 of lower-to-upper range
     # Map fractions back to spectrum bin indices that contribute to column
@@ -87,9 +91,9 @@ for column in range(pixel_width):
     # lower columns slightly and boost higher columns. It graphs better.
     total = sum(bin_weights)
     bin_weights = [
-        (weight / total) * (0.8 + idx / pixel_width * 1.4)
+        (weight / total) * (0.8 + idx / glasses.width * 1.4)
         for idx, weight in enumerate(bin_weights)
-        ]
+    ]
     # List w/five elements is stored for each column:
     # 0: Index of the first spectrum bin that impacts this column.
     # 1: A list of bin weights, starting from index above, length varies.
@@ -101,8 +105,8 @@ for column in range(pixel_width):
         [
             first_bin - low_bin,
             bin_weights,
-            colorwheel(225 * column / pixel_width),
-            pixel_height,
+            colorwheel(225 * column / glasses.width),
+            glasses.height,
             0.0,
         ]
     )
@@ -110,9 +114,9 @@ for column in range(pixel_width):
 
 
 # MAIN LOOP -------------
-# Original dynamic_level = 10
-dynamic_level = 0  # For responding to changing volume levels
-frames, start_time = 0, time.monotonic()  # For frames-per-second calc
+
+dynamic_level = 10  # For responding to changing volume levels
+frames, start_time = 0, monotonic()  # For frames-per-second calc
 
 while True:
     # The try/except here is because VERY INFREQUENTLY the I2C bus will
@@ -145,12 +149,13 @@ while True:
 
         # Apply vertical scale to spectrum data. Results may exceed
         # matrix height...that's OK, adds impact!
-        data = (spectrum - lower) * (7 / (dynamic_level - lower))
+        #data = (spectrum - lower) * (7 / (dynamic_level - lower))
+        data = (spectrum - lower) * ((glasses.height + 2) / (dynamic_level - lower))
 
         for column, element in enumerate(column_table):
             # Start BELOW matrix and accumulate bin weights UP, saves math
             first_bin = element[0]
-            column_top = pixel_height + 1
+            column_top = glasses.height + 1
             for bin_offset, weight in enumerate(element[1]):
                 column_top -= data[first_bin + bin_offset] * weight
 
@@ -163,15 +168,12 @@ while True:
 
             column_top = int(column_top)  #      Quantize to pixel space
             for row in range(column_top):  #     Erase area above column
-                pixel_framebuf.pixel(column, row, 0)
-                # glasses.pixel(column, row, 0)
-            for row in range(column_top, 5):  #  Draw column
-                pixel_framebuf.pixel(column, row, element[2])
+                glasses.pixel(column, row, 0)
+            for row in range(column_top, glasses.height):  #  Draw column
+                glasses.pixel(column, row, element[2])
+            glasses.pixel(column, int(element[3]), 0xE08080)  # Draw peak dot
 
-            pixel_framebuf.pixel(column, int(element[3]), 0xE08080)  # Draw peak dot
-
-        # glasses.show()  # Buffered mode MUST use show() to refresh matrix
-        display = framebufferio.FramebufferDisplay(matrix)
+        glasses.show()  # Buffered mode MUST use show() to refresh matrix
 
         frames += 1
         # print(frames / (monotonic() - start_time), "FPS")
