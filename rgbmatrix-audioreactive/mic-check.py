142a8a92342142928aec1745b78f18f22366f18c
Paul Cutler <paul@paulcutler.org>
2022-07-13 14:23:18 -0500

    Add microphone check / PDM program for future featherwing audioreactive project

---
+++ rgbmatrix-audioreactive/mic-check.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import time
+import array
+import math
+import board
+import audiobusio
+
+
+# Remove DC bias before computing RMS.
+def mean(values):
+    return sum(values) / len(values)
+
+
+def normalized_rms(values):
+    minbuf = int(mean(values))
+    samples_sum = sum(
+        float(sample - minbuf) * (sample - minbuf)
+        for sample in values
+    )
+
+    return math.sqrt(samples_sum / len(values))
+
+
+# Main program - note board.SCL and board.SDA are the pins for the StemmaQT microphone
+mic = audiobusio.PDMIn(board.SCL, board.SDA, sample_rate=16000, bit_depth=16)
+samples = array.array('H', [0] * 160)
+
+
+while True:
+    mic.record(samples, len(samples))
+    magnitude = normalized_rms(samples)
+    print((magnitude,))
+    time.sleep(0.1)
