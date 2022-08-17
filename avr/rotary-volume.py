ef47aaf11b2394dc112a42f8681065b2b2f46aaf
Paul Cutler <paul@paulcutler.org>
2022-08-17 14:38:21 -0500

    Add rotary encoder example and working volume up / down using rotary encoder

---
+++ avr/rotary-volume.py
@@ -0,0 +1,64 @@
+# SPDX-FileCopyrightText: 2021 John Furcean
+# SPDX-License-Identifier: MIT
+
+"""Code based on I2C rotary encoder simple test example."""
+
+import board
+from adafruit_seesaw import seesaw, rotaryio, digitalio
+from secrets import secrets
+import wifi
+import socketpool
+
+
+# Set up Receiver
+HOST = "192.168.1.119"
+PORT = 23
+
+buffer = bytearray(1024)
+
+# wifi.radio.connect(secrets["ssid"], secrets["password"])
+try:
+    pool = socketpool.SocketPool(wifi.radio)
+    s = pool.socket(pool.AF_INET, pool.SOCK_STREAM)
+    s.connect((HOST, PORT))
+except OSError:
+    pool = socketpool.SocketPool(wifi.radio)
+    s = pool.socket(pool.AF_INET, pool.SOCK_STREAM)
+    s.connect((HOST, PORT))
+print("Connected!")
+
+seesaw = seesaw.Seesaw(board.STEMMA_I2C(), addr=0x36)
+
+seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+print("Found product {}".format(seesaw_product))
+if seesaw_product != 4991:
+    print("Wrong firmware loaded?  Expected 4991")
+
+seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
+button = digitalio.DigitalIO(seesaw, 24)
+button_held = False
+
+encoder = rotaryio.IncrementalEncoder(seesaw)
+last_position = 0
+
+while True:
+
+    # negate the position to make clockwise rotation positive
+    position = -encoder.position
+
+    if position != last_position:
+
+        if last_position < position:
+            s.send(b"Z2UP\n")
+        else:
+            s.send(b"Z2DOWN\n")
+        last_position = position
+        print("Position: {}".format(position))
+
+    if not button.value and not button_held:
+        button_held = True
+        print("Button pressed")
+
+    if button.value and button_held:
+        button_held = False
+        print("Button released")
