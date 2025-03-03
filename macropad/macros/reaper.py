8d7d13ce1f0a7896e3ed8a997cc4c57e1d5253f4
Paul Cutler <paul@paulcutler.org>
2025-03-03 14:38:10 -0600

    Update colors for Reaper macros

--- macropad/macros/reaper.py
+++ macropad/macros/reaper.py
@@ -7,24 +7,23 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 app = {
     'name' : 'Reaper',
-    'order': 4,  # Application order on the keyboard
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x0000ff, 'CH_L    ', [Keycode.'[']),
+        (0x1034a6, 'CH_L    ', [Keycode.LEFT_BRACKET]),
         (0x0000ff, 'Home    ', [Keycode.HOME]),
-        (0x0000ff, 'CH_R', [Keycode.']']),
+        (0x1034a6, 'CH_R', [Keycode.RIGHT_BRACKET]),
         # 2nd row ----------
-        (0x0000ff, 'Z_IN    ', [Keycode.PLUS),
-        (0x0000ff, 'Down    ', [Keycode.DOWN_ARROW]),
+        (0x0000ff, 'Z_IN    ', [Keycode.EQUALS]),
+        (0x0000ff, 'End    ', [Keycode.END]),
         (0x0000ff, 'Z_OUT', [Keycode.MINUS]),
         # 3rd row ----------
-        (0xeeeeee, 'Cut    ', [Keycode.Z]),
-        (0x00ff00, 'BRTH    ', [Keycode.A]),
-        (0xeeeeee, 'RIP ', [Keycode.X]),
+        (0xce2029, 'CUT    ', [Keycode.Z]),
+        (0xfffaf0, 'BRTH    ', [Keycode.A]),
+        (0xce2029, 'RIP ', [Keycode.X]),
         # 4th row ----------
-        (0x00ff00, 'Left', [Keycode.LEFT_ALT, Keycode.LEFT_ARROW]),
-        (0x00ff00, 'PG DWN', [Keycode.PAGE_DOWN]),
-        (0x00ff00, 'Right', [Keycode.LEFT_ALT, Keycode.RIGHT_ARROW]),
+        (0x1034a6, 'Left', [Keycode.LEFT_ALT, Keycode.LEFT_ARROW]),
+        (0x00ff00, 'Home', [Keycode.HOME]),
+        (0x1034a6, 'Right', [Keycode.LEFT_ALT, Keycode.RIGHT_ARROW]),
     ]
 }
