d622422a77cffa97a2da4d0484eea99c809d45c8
Paul Cutler <paul@paulcutler.org>
2025-03-23 07:30:21 -0500

    Update Reaper macros

--- macropad/macros/reaper.py
+++ macropad/macros/reaper.py
@@ -10,20 +10,21 @@ app = {
     'macros' : [
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
-        (0x1034a6, 'CH_L    ', [Keycode.LEFT_BRACKET]),
-        (0x0000ff, 'Home    ', [Keycode.HOME]),
+        (0x1034a6, 'CH_L  ', [Keycode.LEFT_BRACKET]),
+        (0x0000ff, 'NEW_CH ', [Keycode.SHIFT, Keycode.M]),
         (0x1034a6, 'CH_R', [Keycode.RIGHT_BRACKET]),
         # 2nd row ----------
-        (0x0000ff, 'Z_IN    ', [Keycode.EQUALS]),
-        (0x0000ff, 'End    ', [Keycode.END]),
-        (0x0000ff, 'Z_OUT', [Keycode.MINUS]),
+        (0x0000ff, 'ZM_IN    ', [Keycode.EQUALS]),
+        (0x0000ff, 'HOME   ', [Keycode.HOME]),
+        (0x0000ff, 'ZM_OUT', [Keycode.MINUS]),
         # 3rd row ----------
         (0xce2029, 'CUT    ', [Keycode.Z]),
         (0xfffaf0, 'BRTH    ', [Keycode.A]),
-        (0xce2029, 'RIP ', [Keycode.X]),
+        (0xce2029, 'RIPL ', [Keycode.X]),
         # 4th row ----------
-        (0x1034a6, 'Left', [Keycode.LEFT_ALT, Keycode.LEFT_ARROW]),
-        (0x00ff00, 'Home', [Keycode.HOME]),
-        (0x1034a6, 'Right', [Keycode.LEFT_ALT, Keycode.RIGHT_ARROW]),
+        (0x1034a6, 'LEFT', [Keycode.LEFT_ALT, Keycode.LEFT_ARROW]),
+        (0x00ff00, 'END', [Keycode.END]),
+        (0x1034a6, 'RIGHT', [Keycode.LEFT_ALT, Keycode.RIGHT_ARROW]),
     ]
 }
+
